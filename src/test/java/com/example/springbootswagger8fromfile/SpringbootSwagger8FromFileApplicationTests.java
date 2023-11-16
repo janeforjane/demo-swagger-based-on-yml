30cbc4c9135c185dd03a25e4b579a2e5e80b7394
janeforjane <janeforjane@mail.ru>
2023-11-16 15:40:13 +0300

    is working

---
+++ src/test/java/com/example/springbootswagger8fromfile/SpringbootSwagger8FromFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootswagger8fromfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootSwagger8FromFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
