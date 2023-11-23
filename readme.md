707257af87b247aa99a799adab71275602f0ee0c
janeforjane <janeforjane@mail.ru>
2023-11-23 23:42:29 +0300

    reade

---
+++ readme.md
@@ -0,0 +1,19 @@
+
+## A Demo Swagger api documentation server
+
+Java 1.8
+
+Springboot 2.1.7
+
+Swagger 2
+
+Docs generated on yml
+
+
+### API UI
+
+http://localhost:8080/swagger-ui.html
+
+### Online editor for doc:
+
+https://editor.swagger.io/
--- src/main/java/com/example/springbootswagger8fromfile/SwaggerUIController.java
+++ src/main/java/com/example/springbootswagger8fromfile/SwaggerUIController.java
@@ -1,7 +1,6 @@
 package com.example.springbootswagger8fromfile;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/swagger")
 public class SwaggerUIController {
 
-    @GetMapping("/{name}/{version}")
-    public String homePage(@PathVariable("name") String name, @PathVariable("version") String version, Model model) {
-        model.addAttribute("filePath", "/swagger/" + name + '/' + version + "/swagger.yaml");
-        return "redoc";
+
+    @GetMapping("/greeting")
+    public String homePage() {
+
+        return "Hi there! For Api - go to host:port/swagger-ui.html";
     }
 
 }
